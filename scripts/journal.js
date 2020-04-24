2f17f9f772e705bfd0863f6b9089eb75943d8ad0
Kirk Sudduth <kirksudduth@gmail.com>
2020-04-24 14:44:52 -0500

    added .js files as per daily-journal 5

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,31 +1,11 @@
+console.log("IF YOU'RE AFRAID TO DO IT, DO IT UNTIL YOU'RE NOT.");
+
 /*
     Define the keys and value for a JavaScript object that
     represents a journal entry about what you learned today
 */
 
 const journalCollection = [];
-
-const journalEntries = [
-  {
-    date: "04/21/2020",
-    concepts: "objects",
-    entry: "it has been an eventful learning day. feeling good about it.",
-    mood: "happy",
-  },
-  {
-    date: "04/21/2020",
-    concepts: "methods",
-    entry:
-      "i understand (kind of) what a method is. which is more than i can say about myself yesterday.",
-    mood: "meh",
-  },
-  {
-    date: "04/21/2020",
-    concepts: "this",
-    entry: "we haven't dove super far into it yet. fringe understanding so far",
-    mood: "confused",
-  },
-];
 
 /*
     Purpose: To create, and return, a string template that
@@ -37,11 +17,10 @@ const makeJournalEntryComponent = (journalEntry) => {
   // Create your own HTML structure for a journal entry
   return `
   <div class="journal_entry">
-  <ul>
-  <li class="concepts">${journalEntry.concepts}</li>
-  <li class="date">${journalEntry.date}</li>
-  <li class="entry">${journalEntry.entry}</li>
-  <li class="mood">${journalEntry.mood}</li></ul></div>
+  <p class="concepts">Concept: ${journalEntry.concepts}</p>
+  <p class="date">Date: ${journalEntry.date}</p>
+  <p class="entry">Entry: ${journalEntry.entry}</p>
+  <p class="mood">Mood: ${journalEntry.mood}</p></div>
 `;
 };
 
@@ -50,12 +29,14 @@ const makeJournalEntryComponent = (journalEntry) => {
 
     Arguments: entries (array of objects)
 */
-const journalContainer = document.querySelector(".entryLog");
-const renderJournalEntries = (entries) => {
-  for (let i = 0; i < journalEntries.length; i++) {
-    const entries = journalEntries[i];
-    journalContainer.innerHTML += makeJournalEntryComponent(entries);
-  }
-};
-// Invoke the render function
-renderJournalEntries(journalEntries);
+// const journalContainer = document.querySelector(".entryLog");
+// const renderJournalEntries = (entries) => {
+//   for (let i = 0; i < journalEntries.length; i++) {
+//     const entries = journalEntries[i];
+//     journalContainer.innerHTML += makeJournalEntryComponent(entries);
+//   }
+// };
+// // Invoke the render function
+// renderJournalEntries(journalEntries);
+
+// Using fetch() to Query Data
