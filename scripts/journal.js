078a9a42947eb5309f4a9e891473888d93b6292f
Kirk Sudduth <kirksudduth@gmail.com>
2020-04-25 13:44:19 -0500

    added the API object to data.js and used it in the journal.js to render journal entries on page

--- scripts/journal.js
+++ scripts/journal.js
@@ -5,38 +5,28 @@ console.log("IF YOU'RE AFRAID TO DO IT, DO IT UNTIL YOU'RE NOT.");
     represents a journal entry about what you learned today
 */
 
-const journalCollection = [];
-
 /*
     Purpose: To create, and return, a string template that
     represents a single journal entry object as HTML
 
     Arguments: journalEntry (object)
 */
-const makeJournalEntryComponent = (journalEntry) => {
-  // Create your own HTML structure for a journal entry
-  return `
-  <div class="journal_entry">
-  <p class="concepts">Concept: ${journalEntry.concepts}</p>
-  <p class="date">Date: ${journalEntry.date}</p>
-  <p class="entry">Entry: ${journalEntry.entry}</p>
-  <p class="mood">Mood: ${journalEntry.mood}</p></div>
-`;
-};
 
 /*
     Purpose: To render all journal entries to the DOM
 
     Arguments: entries (array of objects)
 */
-// const journalContainer = document.querySelector(".entryLog");
-// const renderJournalEntries = (entries) => {
-//   for (let i = 0; i < journalEntries.length; i++) {
-//     const entries = journalEntries[i];
-//     journalContainer.innerHTML += makeJournalEntryComponent(entries);
-//   }
-// };
+
 // // Invoke the render function
-// renderJournalEntries(journalEntries);
 
 // Using fetch() to Query Data
+
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+
+    Change the fake variable names below to what they should be
+    to get the data and display it.
+*/
+API.getJournalEntries().then(renderJournalEntries());
